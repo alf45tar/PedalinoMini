3aa368ed99518c8613b8a9cccd6d8c51ac57e367
alf45tar <alf45tar@gmail.com>
2021-11-12 19:58:52 +0100

    2.3.0

--- src/Pedalino.h
+++ src/Pedalino.h
@@ -14,7 +14,7 @@ __________           .___      .__  .__                 _____  .__       .__    
 #ifndef _PEDALINO_H
 #define _PEDALINO_H
 
-#define VERSION         "2.2.13"
+#define VERSION         "2.3.0"
 
 #define MODEL           "PedalinoMini™"
 #define INTERFACES        6
